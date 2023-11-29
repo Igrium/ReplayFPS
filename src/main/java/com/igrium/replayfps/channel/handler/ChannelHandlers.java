bd773cefdde8a8cd834d1d3f03904896ea2d519b
Sam54123 <thesam54123@gmail.com>
2023-11-28 18:47:21 -0800

    removed unused channel handlers

--- src/main/java/com/igrium/replayfps/channel/handler/ChannelHandlers.java
+++ src/main/java/com/igrium/replayfps/channel/handler/ChannelHandlers.java
@@ -10,16 +10,13 @@ import com.igrium.replayfps.recording.ClientCaptureContext;
 
 import net.minecraft.util.Identifier;
 
-@SuppressWarnings("deprecation")
 public class ChannelHandlers {
     
     public static final BiMap<Identifier, ChannelHandler<?>> REGISTRY = HashBiMap.create();
 
     public static final ChannelHandler<?> DUMMY = register(new DummyChannelHandler(), new Identifier("replayfps:dummy"));
     public static final PlayerPosChannelHandler PLAYER_POS = register(new PlayerPosChannelHandler(), new Identifier("replayfps:player_pos"));
-    public static final PrevPosChannelHandler PREV_POS = register(new PrevPosChannelHandler(), new Identifier("replayfps:prev_pos"));
     public static final PlayerRotChannelHandler PLAYER_ROT = register(new PlayerRotChannelHandler(), new Identifier("replayfps:player_rot"));
-    public static final PrevRotChannelHandler PREV_ROT = register(new PrevRotChannelHandler(), new Identifier("replayfps:prev_rot"));
     public static final PlayerVelocityChannelHandler PLAYER_VELOCITY = register(new PlayerVelocityChannelHandler(), new Identifier("replayfps:player_velocity"));
     public static final PlayerStrideChannelHandler PLAYER_STRIDE = register(new PlayerStrideChannelHandler(), new Identifier("replayfps:player_stride"));
     public static final HorizontalSpeedHandler HORIZONTAL_SPEED = register(new HorizontalSpeedHandler(), new Identifier("replayfps:horizontal_speed"));
