112a5b49f38af1a7889c5e46e28a608e67c208ff
Sam54123 <thesam54123@gmail.com>
2023-11-30 14:44:00 -0800

    food now works properly

--- src/main/java/com/igrium/replayfps/networking/FakePacketHandlers.java
+++ src/main/java/com/igrium/replayfps/networking/FakePacketHandlers.java
@@ -1,12 +1,16 @@
 package com.igrium.replayfps.networking;
 
+import com.igrium.replayfps.game_events.UpdateFoodEvent;
 import com.igrium.replayfps.util.PlaybackUtils;
 
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.listener.PacketListener;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.HealthUpdateS2CPacket;
+import net.minecraft.util.Identifier;
 
 public class FakePacketHandlers {
     public static void register() {
@@ -16,7 +20,19 @@ public class FakePacketHandlers {
             return false;
         });
 
-        
+        // FOOD
+        UpdateFoodEvent.EVENT.register((player, food, saturation) -> {
+            PacketByteBuf buf = PacketByteBufs.create();
+            buf.writeInt(food);
+            buf.writeFloat(saturation);
+
+            CustomReplayPacketManager.sendReplayPacket(new Identifier("replayfps:update_food"), buf);
+        });
+
+        CustomReplayPacketManager.registerReceiver(new Identifier("replayfps:update_food"), (client, buf, player) -> {
+            player.getHungerManager().setFoodLevel(buf.readInt());
+            player.getHungerManager().setSaturationLevel(buf.readFloat());
+        });
     }
 
     private static <T extends Packet<?>> void registerVanillaWithType(Class<T> type, PacketListenerTyped<T> listener) {
