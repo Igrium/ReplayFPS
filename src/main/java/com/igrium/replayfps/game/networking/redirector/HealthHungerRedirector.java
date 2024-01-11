1115636cc0191a46605a456adb126ba0cd1ee402
Sam54123 <thesam54123@gmail.com>
2024-01-11 12:35:26 -0800

    removed health update health-hunger redirector because it's handled in vanilla packets

--- src/main/java/com/igrium/replayfps/game/networking/redirector/HealthHungerRedirector.java
+++ src/main/java/com/igrium/replayfps/game/networking/redirector/HealthHungerRedirector.java
@@ -22,25 +22,8 @@ public class HealthHungerRedirector implements PacketRedirector<HealthUpdateS2CP
     @Override
     public void redirect(HealthUpdateS2CPacket packet, PlayerEntity localPlayer, MinecraftClient client) {
         client.execute(() -> {
-            doUpdateHealth(packet.getHealth(), localPlayer);
             localPlayer.getHungerManager().setFoodLevel(packet.getFood());
             localPlayer.getHungerManager().setSaturationLevel(packet.getSaturation());
         });
     }
-
-    private void doUpdateHealth(float health, PlayerEntity player) {
-        if (health == player.getHealth()) return;
-        float f = player.getHealth() - health;
-        if (f < 0) {
-            player.setHealth(health);
-            player.timeUntilRegen = 10;
-        } else {
-            ((LivingEntityAccessor) player).setLastDamageTaken(f);
-            player.timeUntilRegen = 20;
-            player.setHealth(health);
-            player.maxHurtTime = 10;
-            player.hurtTime = player.maxHurtTime;
-        }
-    }
-    
 }
